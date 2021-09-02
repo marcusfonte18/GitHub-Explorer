abc4ba940629d931d88b47b3f8fd57c69ce8bec1
marcusfonte18 <marcusfonte18@gmail.com>
2021-09-02 00:08:17 -0300

    estilizando o Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,56 @@
 import React from 'react';
-import Title from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import Title, { Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore Repositórios do GitHub</Title>;
+import logoGitHub from '../../assets/img/logo.svg';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoGitHub} alt="GitHub Explorer" />
+    <Title>Explore Repositórios do GitHub</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/65238795?v=4"
+          alt="Marcus"
+        />
+        <div>
+          <strong>Go-stack-GitHub</strong>
+          <p>Fácil de fazer e altamente escalável</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/65238795?v=4"
+          alt="Marcus"
+        />
+        <div>
+          <strong>Go-stack-GitHub</strong>
+          <p>Fácil de fazer e altamente escalável</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/65238795?v=4"
+          alt="Marcus"
+        />
+        <div>
+          <strong>Go-stack-GitHub</strong>
+          <p>Fácil de fazer e altamente escalável</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
