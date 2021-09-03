89c79e07c572225db8789bfc9f4a656256af1b33
marcusfonte18 <marcusfonte18@gmail.com>
2021-09-03 13:34:11 -0300

    estilizando a pagina repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { useRouteMatch } from 'react-router-dom';
+import logoImg from '../../assets/img/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryProps {
   repository: string;
@@ -9,7 +13,54 @@ interface RepositoryProps {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryProps>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/65238795?v=4"
+            alt=""
+          />
+          <div>
+            <strong>rocket/strong</strong>
+            <p>descrição dasdasaggaga</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asjdasj">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
