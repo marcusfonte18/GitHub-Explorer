dfcc05f6597e0371899cf40ff675d004e339370d
marcusfonte18 <marcusfonte18@gmail.com>
2021-09-03 12:38:56 -0300

    navegando entre dashboard e repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,15 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryProps {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryProps>();
+  return <h1>Repository: {params.repository}</h1>;
+};
 
 export default Repository;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
-// import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
 
